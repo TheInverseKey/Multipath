8b94c3d91b2f7d9a35bf008473d5328de2040611
Connor <connor@lyricalsecurity.com>
2017-09-20 21:54:22 -0400

    Equivalent Oops
    '==' NOT '=' on the filter

--- assembler.py
+++ assembler.py
@@ -2,4 +2,4 @@ from scapy.all import *
 
 
 if __name__ == '__main__':
-        sniff(iface = "enp0s3", prn = lambda x: x.show(), filter = "tcp[54] = 30", store = 0)
+        sniff(iface = "enp0s3", prn = lambda x: x.show(), filter = "tcp[54] == 30", store = 0)
