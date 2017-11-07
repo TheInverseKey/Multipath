1c89df52c6c34328208de1ab8bad2ca2ae7b5e21
Unknown <mnsharkey@myseneca.ca>
2017-11-07 12:02:35 -0800

    more testing

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 from scapy.all import *
+from mscapy import *
 from mscapy.layers import *
 from mscapy.layers.inet import TCP
 
 
-#pcap = rdpcap("/home/python/Downloads/testing.pcapng")
-pcap = sniff(offline= '/home/python/Downloads/testing.pcapng', prn=lambda x: x.show(), filter = "tcp[54] == 30", store=0)
-
+a= rdpcap("/home/python/Downloads/testing.pcapng")
+a[0].show2()
