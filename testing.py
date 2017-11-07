95e534f18f0f1a553794b038c374c0cdee5d2090
Connor <connor@lyricalsecurity.com>
2017-11-07 16:34:19 -0500

    Auto stash before merge of "master" and "origin/master"

--- testing.py
+++ testing.py
@@ -1,6 +1,4 @@
-from scapy.all import *
-from mscapy.layers.inet import TCP
-from mscapy import packet as mpacket
+from mscapy.all import *
 mshow2 = mpacket.Packet.show2
 a = rdpcap("/home/python/Downloads/testing.pcapng")
 a[0].packet.show2()
