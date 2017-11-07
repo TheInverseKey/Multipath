195ce6baea39ce3d81d8053440ebe89d80c238b8
Connor <connor@lyricalsecurity.com>
2017-11-07 18:25:25 -0500

    Can get DSN now

--- testing.py
+++ testing.py
@@ -1,12 +1,12 @@
 from scapy.all import *
-a = rdpcap("/home/python/Desktop/testing.pcap")
-a[0].show2()
-
-for pkt in a:
-    for opt in pkt[TCP].options:
-        for o in opt.mptcp:
-            if o.dsn:
-                print o.dsn
+a = rdpcap("./testing.pcap")
+a[0].show()
+pkt = a[0]
+for opt in pkt[TCP].options:
+    try:
+        print opt.mptcp.dsn
+    except:
+        pass
 
 #pkt[MPTCP].dsn
 #sniff(offline='/home/python/Desktop/testing.pcap', prn=lambda x: x.show(), filter = "tcp[54] == 30", store=0)
