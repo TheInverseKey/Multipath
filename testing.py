1075f14391d8b92f6d28ec134b2a712f6436a62e
Connor <connor@lyricalsecurity.com>
2017-11-07 19:25:41 -0500

    structure in the sniffer

---
+++ testing.py
@@ -0,0 +1,21 @@
+from scapy.all import *
+a = rdpcap("./testing.pcap")
+a[0].show()
+pkt = a[0]
+
+for opt in pkt[TCP].options:
+    try:
+        print opt.mptcp.dsn
+    except:
+        pass
+
+#pkt[MPTCP].dsn
+#sniff(offline='/home/python/Desktop/testing.pcap', prn=lambda x: x.show(), filter = "tcp[54] == 30", store=0)
+
+
+
+#def newconvo(pkt):
+    #sniff(iface=interface, prn=lambda x: x.show(), filter = "tcp[54] == 30", store=0)
+#def
+
+
