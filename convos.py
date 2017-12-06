851c53344868d903264e1e3c53fa998d8910ad15
Unknown <mnsharkey@myseneca.ca>
2017-12-06 11:29:29 -0800

    got rid of not needed stuff

--- convos.py
+++ convos.py
@@ -33,10 +33,8 @@ class ConvoHandler(object):
         #pkt.convert()
         #pkt.send()
     def pkt_send(self):
-        eth = Ether(dst=MAC_dst, src=MAC_src, type=eth_type)
+        
         ip = IP(src=src_IP, dst=dst_IP, proto=6, len=length, chksum=chksum)
-        tcp = ip / TCP(sport=sprt, dport=dprt, flags=flag, seq=seq, ack=ack, chksum=tcp_chksum, window=window)
-        payload = data
         send(eth/ip/tcp/payload)
 
 
