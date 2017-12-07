c13b9555ae2dc3b184087ea8e491c276868d85a0
Connor MacLeod <connor.macleod96@hotmail.ca>
2017-12-06 20:42:19 -0500

    Yooo, Handle-Packet and co. work

--- convos.py
+++ convos.py
@@ -1,6 +1,10 @@
 import binascii
+import hashlib
 from scapy.all import *
 from pkt import Packet
+
+#PyCharm Hack
+TCP = TCP
 
 class ConvoHandler(object):
     """
@@ -16,52 +20,59 @@ class ConvoHandler(object):
         Get packet flags/options and then act accordingly
         :param scapy_pkt:
         """
-        pkt = Packet(scapy_pkt)
-        opts = pkt.get_opts()
+        self.pkt = Packet(scapy_pkt)
+        for opt in self.pkt.get_opts():
+            if opt != 'DSS':
+                print 'MPTCP Subtype:', opt
+            if "DSS" in opt:
+                if hasattr(self.pkt, 'dsn'):
+                    self.update_dss(list(self.pkt.addr), self.pkt.dsn, self.pkt.pkt[TCP].seq)
 
-        if "DSS":
-            self.update_dss(pkt.addr, pkt.pkt[TCP].options.mptcp.dsn, pkt.pkt[TCP].seq)
-        if "FIN" in opts:
-            self.teardown(pkt.addr)
-        elif "FINACK" in opts:
-            self.teardown(pkt.addr, end_convo=True)
-        elif "MP_CAPABLE" in opts:
-            self.add_master(pkt.addr, pkt.pkt[TCP].options.mptcp.snd_key)
-            #MP_CAPABLE means a DSN doesn't exist and there is no payload, so why send it?
-            return
-        elif "MP_JOIN" in opts:
-            self.add_subflow(pkt.addr, pkt.pkt[TCP].options.mptcp.rcv_token)
+            if "FIN" in opt:
+                self.teardown(self.pkt.addr)
 
+            if "FINACK" in opt:
+                self.teardown(self.pkt.addr, end_convo=True)
+
+            if "MPCAPABLE" in opt:
+                if hasattr(self.pkt, 'snd_key'):
+                    snd_key = format(self.pkt.snd_key, 'x')
+                    self.add_master(self.pkt.addr, snd_key)
+
+            if "MPJOIN" in opt:
+                if hasattr(self.pkt, 'rcv_token'):
+                    self.add_subflow(self.pkt.addr, format(self.pkt.rcv_token, 'x'))
+
+    def push_packet_as_single_stream(self):
         #TODO Maybe put this in it's own function, idk anymore man
-        if pkt.addr in self.subflows.keys():
-            master_addr = self.subflows[pkt.addr]['master']
-            src, dst = master_addr[0], master_addr[1]
-            try:
-                dsn = self.subflows[pkt.addr]['dsn']
-                pkt.convert(dsn, src=src, dst=dst)
+        if not hasattr(self, 'pkt'):
+            raise AttributeError('You need a packet to do this')
+        try:
+            if self.pkt.addr in self.subflows.keys():
+                master_addr = self.subflows[self.pkt.addr]['master']
+                src, dst = master_addr[0], master_addr[1]
+                dsn = self.subflows[self.pkt.addr]['dsn']
+                self.pkt.convopt.mptcp.sert(dsn, src=src, dst=dst)
 
-            except KeyError:
-                print 'Oh shit, we have a packet but no listed DSN for it!'
-                print 'Here\'s the adress sequence number for reference: \n {} \n {}'.format(pkt.addr, pkt.pkt[TCP].seq)
+            else:
+                dsn = self.master_flows[self.pkt.addr]['dsn']
+                self.pkt.convert(dsn)
 
-        else:
-            try:
-                dsn = self.master_flows[pkt.addr]['dsn']
-                pkt.convert(dsn)
-                
-            except KeyError:
-                print 'Oh shit, we have a packet but no listed DSN for it!'
-                print 'Here\'s the adress sequence number for reference: \n {} \n {}'.format(pkt.addr, pkt.pkt[TCP].seq)
+        except KeyError:
+            print 'Oh shit, we have a packet but no listed DSN for it!'
+            print 'Here\'s the adress sequence number for reference:'
+            print '{} \n {}'.format(self.pkt.addr, self.pkt.pkt[TCP].seq)
 
-        pkt.send()
+        self.pkt.send()
 
 
     def add_master(self, addr, snd_key):
         """
-        Add a new convo to the master_flows dict (and derive token from key)
+        Add a new convo to the master_print opts if opts != 'DSS'flows dict (and derive token from key)
         :param addr:    tuple with (src, dst)
         :param snd_key: hex value of sender's key in packet
         """
+        print 'Add Master:', addr
         generic_addr = frozenset(addr)
         self.convos.add(generic_addr)
         # Derive token from key
@@ -79,13 +90,14 @@ class ConvoHandler(object):
         :param token: receiver's token
         :return:
         """
+        print 'Add Subflow:', addr
         generic_addr = frozenset(addr)
         self.convos.add(generic_addr)
         # Find matching recv_key
-        for flow, info in self.master_flows.items():
+        for flow, info in self.master_flows.iteritems():
             if info['token'] == rcv_token:
-                master_addr = addr[::-1]
-                if self.master_flows[master_addr]:
+                master_addr = flow[::-1]
+                if master_addr in self.master_flows.keys():
                     self.subflows[addr] = {
                         'master': master_addr
                     }
@@ -94,12 +106,14 @@ class ConvoHandler(object):
                           "but we have no record of that flow!".format(addr,master_addr)
 
     def update_dss(self, addr, dsn, seq_num):
-        """
+        # type: (list, int, int) -> None
+        """opt.mptcp.s
         Update a flows DSS map
         :param addr:    tuple (src, dst)
         :param dsn:     int packet dsn
         :param seq_num: int packet sequence number
         """
+        print 'Update DSS:', addr
         generic_addr = frozenset(addr)
         if generic_addr in self.convos:
             dss_dict = {
@@ -107,10 +121,10 @@ class ConvoHandler(object):
                 'diff': dsn - seq_num #TODO examine if this is necessary (I don't think it is)
             }
 
-            if addr in self.master_flows:
+            if addr in self.master_flows.keys():
                 self.master_flows[addr].update(dss_dict)
 
-            elif addr in self.subflows:
+            elif addr in self.subflows.keys():
                 self.subflows[addr].update(dss_dict)
 
         else:
@@ -130,3 +144,15 @@ class ConvoHandler(object):
 
             if end_convo:
                 self.convos.remove(generic_addr)
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pcap = rdpcap('./demo.pcap')
+
+    convo = ConvoHandler()
+    for packet in pcap:
+        convo.handle_packet(packet)
+
+    pprint(convo.master_flows)
+    pprint(convo.subflows)
+    pprint(convo.convos)
--- pkt.py
+++ pkt.py
@@ -1,5 +1,7 @@
 from scapy.all import *
-
+#PyCharm Hack
+TCP=TCP
+IP=IP
 
 class Packet(object):
 
@@ -9,8 +11,8 @@ class Packet(object):
         Rip scapy packet into the bits we need
         :param pkt: A scapy packet
         """
-        self.src = "{}:{}".format(pkt[IP].src, pkt[TCP].sport)
-        self.dst = "{}:{}".format(pkt[IP].dst, pkt[TCP].dport)
+        self.src = (pkt[IP].src, pkt[TCP].sport)
+        self.dst = (pkt[IP].dst, pkt[TCP].dport)
         self.addr = (self.src, self.dst)
         self.pkt = pkt
 
@@ -19,22 +21,31 @@ class Packet(object):
         :return: set of all the options detected within the packet (will usually be 1).
         """
         opts = set()
-        for opt in self.pkt[TCP].options:
-            # If we con't do attr checks it will throw exceptions at us
-            if hasattr(opt, 'mptcp'):
-                if hasattr(opt.mptcp, 'subtype'):
-                    if opt.mptcp.subtype == 2:
-                        opts.add("DSS")
-                    elif opt.mptcp.subtype == 0:
-                        opts.add("MPCAPABLE")
-                    elif opt.mptcp.subtype == 1:
-                        opts.add("MPJOIN")
+        for subtype in self.get_mp_opt('subtype'):
+            if subtype == 2:
+                try:
+                    self.dsn = self.get_mp_opt('dsn').next()
+                except StopIteration:
+                    pass
+                opts.add("DSS")
+            elif subtype == 0:
+                try:
+                    self.snd_key = self.get_mp_opt('snd_key').next()
+                except StopIteration:
+                    pass
+                opts.add("MPCAPABLE")
+            elif subtype == 1:
+                try:
+                    self.rcv_token = self.get_mp_opt('rcv_token').next()
+                except StopIteration:
+                    pass
+                opts.add("MPJOIN")
 
-            if self.pkt[TCP].flags == 0x01:
-                opts.add("FIN")
+        if self.pkt[TCP].flags == 0x01:
+            opts.add("FIN")
 
-            elif self.pkt[TCP].flags == 0x11:
-                opts.add("FINACK")
+        elif self.pkt[TCP].flags == 0x11:
+            opts.add("FINACK")
         return opts
 
     def convert(self, new_seq, src=None, dst=None):
@@ -68,16 +79,38 @@ class Packet(object):
 
         sendp(self.pkt, iface=iface)
 
-    def frag_check(self, threshold):
+    def get_mp_opt(self, attr):
+        """
+        Returns the value(s) of an mptcp sub-option in a scapy packet
+        :param attr: the name of the mptcp option to get the value(s) of
+        :return: the value(s) of the sub-option
+        """
         for opt in self.pkt[TCP].options:
-            if hasattr(opt, "mptcp"):
-                if hasattr(opt.mptcp, "length"):
-                    if opt.mptcp.length <= threshold:
-                        print "Length smaller then %s bytes, possible fragmentation!" % threshold
+            if hasattr(opt, 'mptcp'):
+                if hasattr(opt.mptcp, attr):
+                    yield getattr(opt.mptcp, attr, None)
+
+    def frag_check(self, threshold):
+        mp_length = self.get_mp_opt('length').next()
+        if mp_length <= threshold:
+            print "Length smaller then %s bytes, possible fragmentation!" % threshold
 
 
-if __name__ == '__main__':
-    a = rdpcap("./mpjoin.pcap")
-    pkt = a[1]
+if __name__ == '__m   ain__':
+    import inspect
+    from pprint import pprint
+    a = rdpcap("./demo.pcap")
+    pkt = a[8]
     p = Packet(pkt)
-    p.get_opts()
+    print p.get_opts()
+    p.convert(6969)
+    print p.pkt[TCP].seq
+    for opt in p.pkt[TCP].options:
+        if hasattr(opt, 'mptcp'):
+            if hasattr(opt.mptcp, 'length'):
+                opt.mptcp.length = 1
+    p.frag_check(2)
+    for opt in p.pkt[TCP].options:
+        if opt.kind == 30:
+            print p.pkt[TCP].flags
+            print opt.summary
