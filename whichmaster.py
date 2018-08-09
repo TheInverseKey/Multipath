59db172f8d86750637eceae75dfc4a3f77d9b46e
Connor MacLeod <connor.macleod96@hotmail.ca>
2018-08-08 21:39:22 -0400

    Broken logic
    my lagic was bad and I feel bad

--- whichmaster.py
+++ whichmaster.py
@@ -1,6 +1,7 @@
 import json
 import sys
 
+# ie. $python whichmaster.py "((127.0.0.1, 8080),(127.0.0.1,80))" ip_relationships.json
 query_addr = sys.argv[1]
 filename = sys.argv[2]
 
@@ -8,11 +9,10 @@ def whichmaster(query_addr, filename):
     with open(filename, 'r') as dicts:
         for entry in dicts:
             entry = json.loads(entry)
-            if query_addr in entry.values():
+            if query_addr in entry.values()[0]:
                 return entry.keys()[0]
 
             return "No master found."
-
 
 
 if __name__ == '__main__':
