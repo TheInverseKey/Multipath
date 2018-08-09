70e6d821d9a9f79e86df13d2014f86d1154136b2
Connor MacLeod <connor.macleod96@hotmail.ca>
2018-08-08 21:29:29 -0400

    Removed shitty error handling
    Let the damn thing error if it wants to

--- whichmaster.py
+++ whichmaster.py
@@ -8,14 +8,8 @@ def whichmaster(query_addr, filename):
     with open(filename, 'r') as dicts:
         for entry in dicts:
             entry = json.loads(entry)
-            try:
-                if query_addr in entry.values():
-                    return entry.keys()[0]
-                    break
-            except Exception as e:
-                print "Corrupt IP relationship file."
-                print "Read failed with error: {}".format(e)
-                return e
+            if query_addr in entry.values():
+                return entry.keys()[0]
 
             return "No master found."
 
